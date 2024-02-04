d9f1cfc750586ba4048c6ee6856a48d4367c8e20
marcosd59 <damian.marcospool@gmail.com>
2024-02-04 16:56:14 -0500

    Add product data module

---
+++ src/product-data.js
@@ -0,0 +1,59 @@
+const productos = [
+  {
+    id: 1,
+    name: "Audífonos gamer Onikuma Gamer K9",
+    price: 530.72,
+    stock: 10,
+    rating: 5,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_802756-MLU72755075838_112023-F.webp",
+    description:
+      "¡Experimenta la adrenalina de sumergirte en la escena de otra manera! Tener auriculares específicos para jugar cambia completamente tu experiencia en cada partida. Con los Onikuma K9 no te pierdes ningún detalle y escuchas el audio tal y como fue diseñado por los creadores.",
+  },
+  {
+    id: 2,
+    name: "RGB Mouse Pad Gamer",
+    price: 299.99,
+    stock: 5,
+    rating: 5,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_947683-MLU73783504166_012024-F.webp",
+    description:
+      "PAlfombrilla RGB 300x80x4mm para Gaming Mouse y Teclado, Equipo Profesional y de Moda!",
+  },
+  {
+    id: 3,
+    name: "Razer V3 Pro Ratón Rgb",
+    price: 3499.99,
+    stock: 15,
+    rating: 4,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_707396-CBT71292187165_082023-F.webp",
+    description:
+      "Tecnología de carga inalámbrica Una nueva generación de tecnología de carga inalámbrica que ofrece capacidades de carga rápidas y convenientes. El Razer V3 Pro admite el uso del nuevo Razer Mouse Dock Pro para la carga inalámbrica, y también puedes actualizar la parte inferior del ratón con un módulo de carga inalámbrico para admitir más dispositivos de carga con protocolo Q.",
+  },
+  {
+    id: 4,
+    name: "Monitor",
+    price: 5999.99,
+    stock: 2,
+    rating: 4,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_745571-CBT72703740553_112023-F.webp",
+    description:
+      "Con tecnología de control de luz azul baja y una pantalla antirreflejo, no puede ver lo que se refleja detrás de usted Sin halos ni imágenes borrosas.",
+  },
+  {
+    id: 5,
+    name: "Laptop Game 16in 2 En 1 Intel I7",
+    price: 12599.99,
+    stock: 20,
+    rating: 5,
+    image:
+      "https://http2.mlstatic.com/D_NQ_NP_2X_720513-CBT73354463396_122023-F.webp",
+    description:
+      "Este portátil está repleto de memoria y almacenamiento potentes. Está equipado con memoria de alto ancho de banda DDR4 de 16 GB, que puede proporcionar un mayor rendimiento y capacidad de respuesta. Ya sea ejecutando varias aplicaciones, pestañas del navegador o realizando tareas complejas al mismo tiempo, puede aumentar su productividad. El diseño de gran ancho de banda de la memoria también acelera las transferencias de datos, lo que hace que sus operaciones sean más fluidas y eficientes.",
+  },
+];
+
+export default productos;
