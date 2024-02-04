a71570f3a6ae2b2882023e544bd5f527536c6754
marcosd59 <damian.marcospool@gmail.com>
2024-02-04 16:56:30 -0500

    Refactor Products component to use dynamic rendering of ProductCards

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,56 +3,27 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import ProductCard from "./ProductCard";
+import productos from "../product-data";
 
-const Item = styled(ProductCard)(({ theme }) => ({
-  padding: theme.spacing(3),
-  textAlign: "center",
+const Item = styled("div")(({ theme }) => ({
+  padding: theme.spacing(4),
+  textAlign: "left",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 }));
 
 export default function Products() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={4}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Item>
-            <ProductCard />
-          </Item>
-        </Grid>
+      <Grid container spacing={1}>
+        {productos.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={product.id}>
+            <Item>
+              <ProductCard product={product} />
+            </Item>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
