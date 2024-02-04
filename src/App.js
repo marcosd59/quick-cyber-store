aa823b44e730f2a0c4ecaae7e911015b3ebd1f10
marcosd59 <damian.marcospool@gmail.com>
2024-02-04 16:56:05 -0500

    Add CheckoutPage component to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import { Check } from "@mui/icons-material";
 import "./App.css";
 import NavBar from "./components/NavBar";
 // import ProductCard from "./components/ProductCard";
-import Products from "./components/Products";
+// import Products from "./components/Products";
+import CheckoutPage from "./components/CheckoutPage";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Products />
+      {/* <Products /> */}
       {/* <ProductCard /> */}
+      <CheckoutPage />
     </div>
   );
 }
