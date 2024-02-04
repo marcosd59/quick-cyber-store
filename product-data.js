84d6ed381533615573c52b2597818737aeafcdb6
marcosd59 <damian.marcospool@gmail.com>
2024-02-04 13:37:40 -0500

    Add product data file

---
+++ product-data.js
@@ -0,0 +1,11 @@
+const products = [
+  {
+    id: 1,
+    name: "Audifonos",
+    price: 499,
+    stock: 10,
+    rating: 4.5,
+    image: "https://via.placeholder.com/150",
+    description: "Product 1 description",
+  },
+];
