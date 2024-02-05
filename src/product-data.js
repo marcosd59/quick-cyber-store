186beb0d9a0417e1afa04775bcbcd65ca21dfddf
marcosd59 <damian.marcospool@gmail.com>
2024-02-05 08:20:50 -0500

    Add new products to product-data.js

--- src/product-data.js
+++ src/product-data.js
@@ -1,3 +1,6 @@
+// import uuid from "react-uuid";
+// uuid();
+
 const productos = [
   {
     id: 1,
@@ -34,7 +37,7 @@ const productos = [
   },
   {
     id: 4,
-    name: "Monitor",
+    name: "Monitor Gamer Curvo",
     price: 5999.99,
     stock: 2,
     rating: 4,
@@ -53,6 +56,77 @@ const productos = [
       "https://http2.mlstatic.com/D_NQ_NP_2X_720513-CBT73354463396_122023-F.webp",
     description:
       "Este portátil está repleto de memoria y almacenamiento potentes. Está equipado con memoria de alto ancho de banda DDR4 de 16 GB, que puede proporcionar un mayor rendimiento y capacidad de respuesta. Ya sea ejecutando varias aplicaciones, pestañas del navegador o realizando tareas complejas al mismo tiempo, puede aumentar su productividad. El diseño de gran ancho de banda de la memoria también acelera las transferencias de datos, lo que hace que sus operaciones sean más fluidas y eficientes.",
+  },
+  {
+    id: 6,
+    name: "Camiseta GitHub de manga corta",
+    price: 299.99,
+    stock: 10,
+    rating: 5,
+    image:
+      "https://down-br.img.susercontent.com/file/sg-11134201-7rblt-lokimn87vnto70",
+    description:
+      "Camiseta de manga corta con el logo de GitHub, ideal para programadores y amantes de la tecnología.",
+  },
+  {
+    id: 7,
+    name: "Sudadera con capucha de Python",
+    price: 499.99,
+    stock: 5,
+    rating: 5,
+    image:
+      "https://m.media-amazon.com/images/I/B1i3u9-Q-KS._CLa%7C2140%2C2000%7CB1mbk1sOoUS.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UY1000_.png",
+    description:
+      "Sudadera con capucha de Python, ideal para programadores y amantes de la tecnología.",
+  },
+  {
+    id: 8,
+    name: "Alfombrilla de ratón grande para juegos",
+    price: 899.99,
+    stock: 3,
+    rating: 5,
+    image: "https://i.ebayimg.com/images/g/gTsAAOSw9sBdEsYe/s-l500.jpg",
+    description:
+      "Alfombrilla de ratón grande para juegos, ideal para programadores y amantes de la tecnología.",
+  },
+  {
+    id: 9,
+    name: "Ocelot Gaming Gabinete para PC",
+    price: 1299.99,
+    stock: 10,
+    rating: 5,
+    image: "https://m.media-amazon.com/images/I/61COk9o2CvL._AC_SX522_.jpg",
+    description:
+      "Gabinete para PC, ideal para programadores y amantes de la tecnología.",
+  },
+  {
+    id: 10,
+    name: "Intel CPU Core i9-14900K 3.2GHz 24core",
+    price: 11799.99,
+    stock: 2,
+    rating: 5,
+    image:
+      "https://m.media-amazon.com/images/I/61ufrInXMuL._AC_UF894,1000_QL80_.jpg",
+  },
+  {
+    id: 11,
+    name: "Funda Anime Demon Slayer Para Airpods 3 2 Pro",
+    price: 99.99,
+    stock: 20,
+    rating: 5,
+    image:
+      "https://down-mx.img.susercontent.com/file/2942a02bb719eb92fc0bd64558dd455a",
+    description: "Funda Anime Demon Slayer Para Airpods 3 2 Pro",
+  },
+  {
+    id: 12,
+    name: "50 Pegatinas De Programación De Tecnología De Software",
+    price: 99.99,
+    stock: 50,
+    rating: 5,
+    image:
+      "https://down-mx.img.susercontent.com/file/a36d81f7e01098a90e2989aff8b3512e",
+    description: "50 Pegatinas De Programación De Tecnología De Software",
   },
 ];
 
