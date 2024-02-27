0519d6cd066ef660d0149593dd776340bb95617c
marcosd59 <damian.marcospool@gmail.com>
2024-02-27 00:02:24 -0500

    Refactor NavBar component and add custom styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -12,19 +12,19 @@ import { Badge } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { useStateValue } from "../StateProvider";
 import { actionTypes } from "../reducer";
-import { auth } from "../firebase"
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { auth } from "../firebase";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 import "../stylesheets/NavBar.css";
+import { styled } from "@mui/material/styles";
 
 export default function NavBar() {
   const [{ basket, user }, dispatch] = useStateValue();
   const history = useNavigate();
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: light)");
   const theme = useTheme(); // Acceso al tema actual
   const [darkMode, setDarkMode] = React.useState(prefersDarkMode); // Estado local para el modo oscuro
-
   const toggleDarkMode = () => {
     setDarkMode(!darkMode); // Cambiar el estado del modo oscuro
   };
@@ -33,14 +33,14 @@ export default function NavBar() {
     () =>
       createTheme({
         palette: {
-          mode: darkMode ? 'dark' : 'light',
+          mode: darkMode ? "dark" : "light",
         },
       }),
     [darkMode]
   );
 
-  const handleAuth = () =>{
-    if (user){
+  const handleAuth = () => {
+    if (user) {
       auth.signOut();
       dispatch({
         type: actionTypes.EMPTY_BASKET,
@@ -50,15 +50,26 @@ export default function NavBar() {
         type: actionTypes.SET_USER,
         user: null,
       });
-      history("/")
+      history("/");
     }
-  }
+  };
+
+  const CustomIconButton = styled(IconButton)({
+    color: "#EBC446",
+  });
+
+  const CustomBadge = styled(Badge)({
+    "& .MuiBadge-badge": {
+      backgroundColor: "purple",
+      color: "#fff",
+    },
+  });
 
   return (
     <ThemeProvider theme={calculatedTheme}>
       <CssBaseline />
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed">
+        <AppBar position="fixed" sx={{ backgroundColor: "#212F77" }}>
           <Toolbar>
             <Link to="/">
               <IconButton>
@@ -69,42 +80,59 @@ export default function NavBar() {
                 />
               </IconButton>
             </Link>
-            <Typography variant="h7" component="div" sx={{ flexGrow: 1, color: darkMode ? 'yellow' : 'white' }}>
+            <Typography
+              variant="h6"
+              component="div"
+              className="quickCyberStore"
+              sx={{ flexGrow: 1, color: "white" }}
+            >
               QuickCyber Store
             </Typography>
-
             <Typography
               variant="h6"
               color="white"
               component="div"
-              sx={{ flexGrow: 1, color: darkMode ? 'yellow' : 'white' }}
+              className="greetingText"
+              sx={{ flexGrow: 1, color: darkMode ? "white" : "white" }}
             >
-               Hello {user ? user.email : "Guest"} 
+              Hello {user ? user.email : "Guest"}
             </Typography>
-            
             <div className={ClassNames.button}>
               <Link to="/signin">
-                <Button color="info" variant="contained" onClick={handleAuth} sx={{ color: darkMode ? 'white' : 'white' }}>
-                  <strong>{user ? "Sign Out" : "Sign In" }</strong>
+                <Button
+                  color="info"
+                  variant="contained"
+                  onClick={handleAuth}
+                  sx={{
+                    color: darkMode ? "white" : "white",
+                    backgroundColor: darkMode ? "#EBC446" : "#EBC446",
+                  }}
+                >
+                  <strong>{user ? "Sign Out" : "Sign In"}</strong>
                 </Button>
               </Link>
 
               <Link to="/checkout-page">
-                <IconButton aria-label="show cart items" color="secondary">
-                  <Badge badgeContent={basket?.length} color="secondary">
+                <CustomIconButton aria-label="show cart items" color="error">
+                  <CustomBadge badgeContent={basket?.length} color="secondary">
                     <ShoppingCart fontSize="large" />
-                  </Badge>
-                </IconButton>
+                  </CustomBadge>
+                </CustomIconButton>
               </Link>
 
-              <Button onClick={toggleDarkMode} sx={{ color: darkMode ? 'yellow' : 'white' }}>
-                {darkMode ? 'Light Mode' : 'Dark Mode'}
+              <Button
+                onClick={toggleDarkMode}
+                sx={{ color: darkMode ? "white" : "white" }}
+              >
+                {darkMode ? "Light Mode" : "Dark Mode"}
               </Button>
             </div>
           </Toolbar>
         </AppBar>
         <div style={{ height: "50px" }}></div>
-        <div style={{ marginTop: "50px" }}>{/* Contenido de los objetos */}</div>
+        <div style={{ marginTop: "50px" }}>
+          {/* Contenido de los objetos */}
+        </div>
       </Box>
     </ThemeProvider>
   );
