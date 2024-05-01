241b2684527b9dd3beb9a79d292426609e4e3fe4
marcosd59 <damian.marcospool@gmail.com>
2024-05-01 15:16:05 -0500

    Update PaymentForm component to use Stripe for payment processing

--- src/components/checkoutform/PaymentForm.jsx
+++ src/components/checkoutform/PaymentForm.jsx
@@ -1,65 +1,146 @@
-import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+import React from "react";
+import Review from "./Review";
+import {
+  Elements,
+  CardElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Button, CircularProgress, Divider, Typography } from "@mui/material";
+import {
+  actionTypes,
+  getBasketTotal,
+  getProductItems,
+  getPorductsNames,
+  getShippingData,
+} from "../../reducer";
+import { useStateValue } from "../../StateProvider";
+import accounting from "accounting";
+import axios from "axios";
+// import { set } from "firebase/database";
 
-export default function PaymentForm() {
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const CARD_ELEMENT_OPTIONS = {
+  iconStyle: "solid",
+  hidePostalCode: true,
+  style: {
+    base: {
+      iconColor: "#212F77",
+      color: "#212F77",
+      fontSize: "16px",
+      "::placeholder": {
+        color: "#212F77",
+      },
+    },
+    invalid: {
+      color: "#9e2146",
+      ":focus": {
+        color: "#fa755a",
+      },
+    },
+  },
+};
+
+const CheckoutForm = ({ backStep, nextStep }) => {
+  const [{ basket, shippingData }, dispatch] = useStateValue();
+  const [loading, setLoading] = React.useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
+    });
+    setLoading(true);
+    if (!error) {
+      const { id } = paymentMethod;
+      try {
+        const { data } = await axios.post(
+          "http://localhost:3001/api/checkout",
+          {
+            id,
+            amount: getBasketTotal(basket) * 100,
+            items: getProductItems(basket),
+            names: getPorductsNames(basket),
+            shippingData: getShippingData(shippingData),
+          }
+        );
+        dispatch({
+          type: actionTypes.SET_PAYMENT_MESSAGE,
+          paymentMessage: data.message,
+        });
+        if (data.message === "Payment successful") {
+          dispatch({
+            type: actionTypes.EMPTY_BASKET,
+            basket: [],
+          });
+        }
+        elements.getElement(CardElement).clear();
+        nextStep();
+      } catch (error) {
+        console.log(error);
+        nextStep();
+      }
+      setLoading(false);
+    }
+  };
+
   return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement options={CARD_ELEMENT_OPTIONS} />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "1rem",
+          }}
+        >
+          <Button variant="outlined" onClick={backStep}>
+            Back
+          </Button>
+          <Button
+            disabled={false}
+            type="submit"
+            variant="contained"
+            sx={{
+              backgroundColor: "#212F77",
+              ":hover": { backgroundColor: "#800080", color: "#FFF" },
+            }}
+          >
+            {loading ? (
+              <CircularProgress
+                sx={{
+                  color: "#EBC446",
+                }}
+              />
+            ) : (
+              "Pay " + accounting.formatMoney(getBasketTotal(basket)) + "$ MXN"
+            )}
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+const PaymentForm = ({ backStep, nextStep }) => {
+  return (
+    <>
+      <Review />
+      <Divider />
+      <Typography variant="h6" gutterBottom sx={{ marginTop: "1rem" }}>
         Payment method
       </Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardName"
-            label="Name on card"
-            fullWidth
-            autoComplete="cc-name"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cardNumber"
-            label="Card number"
-            fullWidth
-            autoComplete="cc-number"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="expDate"
-            label="Expiry date"
-            fullWidth
-            autoComplete="cc-exp"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <TextField
-            required
-            id="cvv"
-            label="CVV"
-            helperText="Last three digits on signature strip"
-            fullWidth
-            autoComplete="cc-csc"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-            label="Remember credit card details for next time"
-          />
-        </Grid>
-      </Grid>
-    </React.Fragment>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm backStep={backStep} nextStep={nextStep} />
+      </Elements>
+    </>
   );
-}
+};
+
+export default PaymentForm;
