b901d0c9177df4df9d9449116e702db7484a6f10
marcosd59 <damian.marcospool@gmail.com>
2024-05-01 15:14:57 -0500

    Add Stripe integration for payment processing

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const Stripe = require("stripe");
+const cors = require("cors");
+const serverlessExpress = require("serverless-express");
+require("dotenv").config();
+
+const stripe = new Stripe(process.env.REACT_APP_STRIPE_SECRET_KEY);
+
+const app = express();
+
+app.use(cors({ origin: "https://quick-cyber-store.netlify.app/" }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+  console.log(req.body);
+  const { id, amount, items, names, shippingData } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "MXN",
+      description: `Purchase of ${names} with ${items} items. Shipping to ${shippingData}`,
+      payment_method: id,
+      confirm: true,
+      payment_method_types: ["card"],
+      return_url: "https://quick-cyber-store.netlify.app/payment-success",
+    });
+    console.log(payment);
+    return res.status(200).json({ message: "Payment successful" });
+  } catch (error) {
+    return res.json({ message: error.raw.message });
+  }
+});
+
+app.use("/.netlify/functions/index", app);
+export const handler = serverlessExpress(app);
